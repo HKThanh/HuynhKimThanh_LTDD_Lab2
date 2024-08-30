1dd822a54e0a1763d4d6fe624d7c549ea39cad5b
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-08-30 08:43:48 +0700

    week2 js ex28 in 50 challenges newbie

--- SourceCodes/50_Challenges_Newbie_27_30.js
+++ SourceCodes/50_Challenges_Newbie_27_30.js
@@ -10,8 +10,32 @@ function distinctArray(arr) {
     return newArr;
 }
 
-console.log(distinctArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3]));
+// console.log(distinctArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3]));
 // 28. Calculate the sum of first 100 prime numbers and return them in an array
+let isPrime = (numbers) => {
+    if (numbers < 2) return true;
+    for (let i = 2; i <= Math.sqrt(numbers); i++) {
+        if (numbers % i === 0)
+            return false;
+    }
+
+    return true;
+}
+
+let first100PrimeNumbers = () => {
+    let i = 0;
+    let array = [];
+    while (i <= 100) {
+        if (isPrime(i))
+            array.push(i);
+        i++;
+    }
+    return array;
+}
+let result = first100PrimeNumbers();
+console.log(result);
+let sum = first100PrimeNumbers().reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(`Sum of first 100 prime numbers: ${sum}`);
 // 29. Print the distance between the first 100 prime numbers
 // 30. Create a function that will add two positive numbers of indefinite size. The
 // numbers
